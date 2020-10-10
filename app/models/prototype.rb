05f7b02e644d4b515c925fa01cb5c4dc440ed020
yamadanatsuko0720 <makinonatsuko@yahoo.co.jp>
2020-10-11 00:11:09 +0900

    userパスの修正

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,5 @@
 class Prototype < ApplicationRecord
-  belongs_to :user , dependent: :destroy
+  belongs_to :user 
   has_one_attached :image
   has_many :comments , dependent: :destroy
   validates :title, presence: true
